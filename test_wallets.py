267bcf47f5572df85a688c34f72cb2efd4161473
mflaxman <mflaxman@gmail.com>
2014-01-15 07:52:56 -0800

    Update test_wallets.py

--- test_wallets.py
+++ test_wallets.py
@@ -6,7 +6,9 @@
 import csv
 from subprocess import check_output
 
+# update this with your filename:
 FILE_NAME = '/media/wipeout/btc_keys_20140113_2058.csv'
+# depending on your installation, you may have to call `bitcoin-tool` with full path
 SHELL_STRING = '''bitcoin-tool \
                     --input-type private-key \
                     --input-format base58check \
@@ -21,8 +23,8 @@ print 'Checking all wallets in %s...' % FILE_NAME
 
 with open(FILE_NAME, 'r') as f:
     for cnt, row in enumerate(csv.DictReader(f)):
-        assert row['public_bitcoin_address'] == get_address_from_wif(row['wif'])
         if cnt % 500 == 0:
             print 'Checking wallet #%s...' % cnt
+        assert row['public_bitcoin_address'] == get_address_from_wif(row['wif'])
 
 print 'All %s wallets were tested and are valid!' % cnt+1
